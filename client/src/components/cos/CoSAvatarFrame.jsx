f82f60136041070f6c95885adb21cdf0e240a9e4
[._.]/ Adam Eivy <atomantic@gmail.com>
2026-09-14 19:33:25 +0000

    fix: unconstrain chief of staff avatar width to prevent canvas clipping

--- client/src/components/cos/CoSAvatarFrame.jsx
+++ client/src/components/cos/CoSAvatarFrame.jsx
@@ -6,7 +6,7 @@ export default function CoSAvatarFrame({ children, label = 'Interactive 3D avata
     <div
       className={background
         ? 'relative w-full h-full min-h-full cursor-grab active:cursor-grabbing touch-none'
-        : 'relative w-full max-w-[8rem] lg:max-w-[12rem] aspect-[5/6] cursor-grab active:cursor-grabbing touch-none'
+        : 'relative w-full aspect-[5/6] cursor-grab active:cursor-grabbing touch-none'
       }
       {...a11yProps}
     >
