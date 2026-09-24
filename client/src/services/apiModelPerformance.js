45c2cbe86cc295d94f65381bb32e6fe67f9429f9
[._.]/ Adam Eivy <atomantic@gmail.com>
2026-09-24 00:56:05 -0700

    fix: make performance API routes statically visible

--- client/src/services/apiModelPerformance.js
+++ client/src/services/apiModelPerformance.js
@@ -1,13 +1,11 @@
 import { request } from './apiCore.js';
 
-const PATH = '/models/performance/task-benchmark';
+export const getModelPerformanceBenchmarks = (options) => request('/models/performance/task-benchmark', options);
 
-export const getModelPerformanceBenchmarks = (options) => request(PATH, options);
-
-export const discoverModelPerformanceModels = (providerId, options) => request(`${PATH}/discover`, {
+export const discoverModelPerformanceModels = (providerId, options) => request('/models/performance/task-benchmark/discover', {
   method: 'POST', body: JSON.stringify({ providerId }), ...options,
 });
 
-export const runModelPerformanceBenchmark = (selection, options) => request(`${PATH}/run`, {
+export const runModelPerformanceBenchmark = (selection, options) => request('/models/performance/task-benchmark/run', {
   method: 'POST', body: JSON.stringify(selection), ...options,
 });
