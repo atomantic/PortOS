fbd6eaa277215b0b8417a9ea7438f6a17c1cdfe6
[._.]/ Adam Eivy <atomantic@gmail.com>
2026-09-26 00:52:11 +0000

    refactor: remove obsolete native status API wrapper (#8549)

--- client/src/services/apiApps.js
+++ client/src/services/apiApps.js
@@ -184,8 +184,7 @@ export const deleteApp = (id) => request(`/apps/${id}`, { method: 'DELETE' });
 // App actions
 export const launchNativeApp = (id, options = {}) =>
   request(`/apps/${id}/native-launch`, { method: 'POST', ...options });
-export const getNativeLaunchStatus = (id, options = {}) =>
-  request(`/apps/${id}/native-launch/status`, options);
+
 export const startApp = (id, options = {}) =>
   request(`/apps/${id}/start`, { method: 'POST', ...options });
 export const stopApp = (id) => request(`/apps/${id}/stop`, { method: 'POST' });
