7550b913066e13831a584edf09b494b25a1164fa
[._.]/ Adam Eivy <atomantic@gmail.com>
2026-09-26 01:15:01 +0000

    fix: remove unused Code Animation client wrapper (#8517)

--- client/src/services/apiCodeAnimation.js
+++ client/src/services/apiCodeAnimation.js
@@ -26,8 +26,6 @@ export const startCodeAnimationGeneration = (brief, options) => request('/code-a
   ...options,
 });
 
-export const listCodeAnimationJobs = (options) => request('/code-animation/jobs', options);
-
 export const listCodeAnimationJobPage = ({ cursor, signal, limit = 50 } = {}) =>
   request(`/code-animation/jobs?limit=${limit}${cursor ? `&cursor=${encodeURIComponent(cursor)}` : ''}`,
     { signal, silent: true });
